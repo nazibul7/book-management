9cf070575749bdd731d630114ed06ea3e9e3adc7
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2024-04-08 00:32:43 +0530

    Deleted unwanted folders

--- contextApi/src/contexts/CounterContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-export const CounterContext = createContext({ count: 0, addCount: () => { }, deleteCount: () => { } })
-
-export const CounterContextProvider = CounterContext.Provider
