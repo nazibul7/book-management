fe319f60815437a7083883c978ba6a4daddcf0e3
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2024-11-30 23:43:52 +0530

    add prisma in user controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express"
 import Apierror from "../utils/apiError"
 import { asyncHandler } from "../middlewares/asyncHandler"
-import { client } from "../db/connectDB"
+import { prisma } from "../db/connectDB"
 import ApiResponse from "../utils/apiResponse"
 import { comparepassword, hashPassword } from "../utils/bcryptUtils"
 import { z } from "zod"
-import { generateAccessToken, generateRefreshToken } from "../utils/tokens"
+import { generateAccessToken } from "../utils/tokens"
 const registrationSchema = z.object({
     name: z.string({
         required_error: "Name is required",
@@ -22,18 +22,32 @@ const registrationSchema = z.object({
 })
 
 const registerUser = asyncHandler(async (req: Request, res: Response) => {
-    const { name, age, email, password } = registrationSchema.parse(req.body)
-    const existingUser = await client.query(`SELECT * FROM authors WHERE email = $1`, [email])
-    if (existingUser.rows.length > 0) {
-        console.log(existingUser.rowCount);
-
-        throw new Apierror({ statusCode: 409, message: "User is already registered!", success: false })
+    try {
+        const { name, age, email, password } = registrationSchema.parse(req.body)
+        const existingUser = await prisma.user.findFirst({
+            where: { email: email }
+        })
+        if (existingUser) {
+            throw new Apierror({ statusCode: 409, message: "User is already registered!", success: false })
+        }
+        const hashingPassword = await hashPassword(password)
+        const accesstoken = generateAccessToken({
+            name: name,
+            email: email
+        }, process.env.ACCESS_TOKEN_SECRET, process.env.ACCESS_TOKEN_EXPIREY)
+        const newUser = await prisma.user.create({
+            data: {
+                name, age, email, password: hashingPassword,accesstoken
+            }
+        })
+        const { password: pass, ...user } = newUser
+        
+        res.status(201).json(
+            new ApiResponse({ message: "User successfully registered", statusCode: 200, success: true, data: [user, accesstoken] })
+        )
+    } catch (error) {
+        return new Apierror({ message: "Something went wrong", statusCode: 500, success: false })
     }
-    const hashingPassword = await hashPassword(password)
-    const newUser = await client.query(`INSERT INTO authors (name,age,email,password) VALUES($1,$2,$3,$4)`, [name, age, email, hashingPassword])
-    res.status(201).json(
-        new ApiResponse({ message: "User successfully registered", statusCode: 200, success: true, data: newUser.fields })
-    )
 })
 
 const loginSchema = z.object({
@@ -42,12 +56,13 @@ const loginSchema = z.object({
 })
 const loginUser = asyncHandler(async (req: Request, res: Response) => {
     const { email, password } = loginSchema.parse(req.body)
-    const existingUser = await client.query(`SELECT * FROM authors WHERE email = $1`, [email])
-    if (existingUser.rows.length == 0) {
-        console.log(existingUser.rowCount);
+    const existingUser = await prisma.user.findFirst({
+        where: { email }
+    })
+    if (!existingUser) {
         throw new Apierror({ statusCode: 409, message: "User is not registered!", success: false })
     }
-    const comparePassword = await comparepassword(password, existingUser.rows[0].password)
+    const comparePassword = await comparepassword(password, existingUser.password)
     if (!comparePassword) {
         throw new Apierror({
             statusCode: 401,
@@ -57,15 +72,10 @@ const loginUser = asyncHandler(async (req: Request, res: Response) => {
     }
 
     const accessToken = generateAccessToken({
-        id: existingUser.rows[0].authorid,
-        name: existingUser.rows[0].name,
-        email: existingUser.rows[0].email
+        id: existingUser.id,
+        name: existingUser.name,
+        email: existingUser.email
     }, process.env.ACCESS_TOKEN_SECRET, process.env.ACCESS_TOKEN_EXPIREY)
-    const refreshToken = generateRefreshToken({
-        id: existingUser.rows[0].authorid
-    }, process.env.REFRESH_TOKEN_SECRET, process.env.REFRESH_TOKEN_EXPIREY)
-
-    await client.query(`UPDATE authors SET refresh_token=$1 WHERE authorid=$2`, [refreshToken, existingUser.rows[0].authorid])
 
     const options = {
         httpOnly: true,
@@ -73,17 +83,16 @@ const loginUser = asyncHandler(async (req: Request, res: Response) => {
     }
     return res.status(200)
         .cookie("accessToken", accessToken, options)
-        .cookie("refreshToken", refreshToken, options)
         .cookie("checkToken", 'true', {
             maxAge: 24 * 60 * 60 * 1000,
-            secure:true
+            secure: true
         })
         .json(
             new ApiResponse({
                 message: "User successfully logged in",
                 statusCode: 200,
                 success: true,
-                data: { refreshToken, accessToken }
+                data: { accessToken }
             })
         )
 })
@@ -92,14 +101,20 @@ interface Extendedrequest extends Request {
     user?: any
 }
 const logoutUser = asyncHandler(async (req: Extendedrequest, res: Response) => {
-    const user = req.user.id
-    await client.query(`UPDATE authors SET refresh_token=NULL WHERE authorid=$1`, [user])
+    const userId = req.user.id
+    await prisma.user.update({
+        where: {
+            id: userId
+        },
+        data: {
+            accesstoken: ''
+        }
+    })
     const options = {
         httpOnly: true,
         secure: true
     }
     res.clearCookie("accessToken", options)
-        .clearCookie("refreshToken", options)
         .clearCookie("checkToken", { secure: true })
         .status(200)
         .json(
